c3dd755c233e4630ff9aff35bbf431c1777930b7
git-spring <dw199501@163.com>
2020-11-29 17:08:42 +0800

    module

--- 03module/Os.py
+++ 03module/Os.py
@@ -1,13 +1,60 @@
-# os   提供调用操作系统的方法
+# os 模块提供了多数操作系统的功能接口函数
+# 当os模块被导入后,它会自适应于不同的操作系统平台,根据不同的平台进行相应的操作
 
 import os
+import stat
 
 
-print(os.name)
-print(os.sep)
+# 查看正在使用的工作平台
+os.name
+# 查看当前所在路径
+os.getcwd()
+# 列出指定目录下所有的文件和目录名, . 表示当前目录  .. 表示上级目录
+os.listdir('E:\\05pythonProject\Pyfox\\03module')
+# 删除指定的文件,文件不存在会报错
+os.remove('Test.py')
+os.unlink('Test.py')
+# 删除指定目录,只能删除空目录  删除非空目录, 使用shutil.rmtree()
+os.rmdir('../Test')
+# 删除多层目录
+os.removedirs('abc/test')
+# 创建目录 只能创建一层
+os.mkdir('test')
+# 创建多层目录
+os.makedirs('abc/test')
+# 重命名文件
+os.rename('Test.py','Test1.py')
+# 改变当前工作目录
+os.chdir('..')
+# 查看文件属性
+os.stat('Test1.py')
+# 修改文件权限
+os.chmod('Test1.py',stat.S_IWOTH)
 
-print(os.path)
-print(os.path.abspath("BuildinMoudle.py"))
-print(os.path.isdir("03module"))
-print(os.path.isfile("BuildinMoudle.py"))
-print(os.path.split("BuildinMoudle.py"))
+
+
+## os.path
+
+# 将文件路径和文件名分割(会将最后一个目录作为文件名而分离)
+os.path.split('E:\\05pythonProject\Pyfox\\03module\Test.py')  # ('E:\\05pythonProject\\Pyfox\\03module', 'Test.py')
+# 将文件路径和文件扩展名分割成一个元组
+os.path.splitext('E:\\05pythonProject\Pyfox\\03module\Test.py')  # ('E:\\05pythonProject\\Pyfox\\03module\\Test', '.py')
+# 返回路径的目录部分
+dirname = os.path.dirname('E:\\05pythonProject\Pyfox\\03module\Test.py')   # E:\05pythonProject\Pyfox\03module
+# 返回路径的文件名部分
+basename = os.path.basename('E:\\05pythonProject\Pyfox\\03module\Test.py')   # Test.py
+# 将文件路径和文件名凑成完整文件路径
+os.path.join(dirname,basename)
+# 获取文件的绝对路径
+os.path.abspath('Test.py')
+# 判断是否是绝对路径
+os.path.isabs('Test.py')
+# 获取文件或者目录是否存在
+os.path.exists('../03module')
+# 判断是否是目录
+os.path.isdir('../03module')
+# 判断是否是文件
+os.path.isfile('Test.py')
+# 返回文件大小
+os.path.getsize('Test.py')
+
--- 03module/Sys.py
+++ 03module/Sys.py
@@ -1,4 +1,28 @@
-# sys  系统相关的方法
+# sys  模块提供了一系列有关Python运行环境的变量和函数
 
 import sys
-sys.stdin
+
+# 命令行参数list 第
+arg0 = sys.argv[0]   # 第 0 个是文件本身
+arg1 = sys.argv[1]   # 第 1 个参数
+arg2 = sys.argv[2]   # 第 2 个参数
+# 返回模块的搜索路径,初始化时使用PYTHONPATH环境变量的值 ( 引用的模块会从这些路径下去寻找)
+dir_list = sys.path
+# 返回所有导入的模块的列表
+sys.modules.keys()
+# 返回系统导入的模块字段,key是模块名,value是模块
+sys.modules
+# 获取python 解释器的版本信息
+sys.version
+# 返回操作平台名称
+sys.platform
+# 标准输入,可以循环输入
+sys.stdin.read()
+# 标准输出
+sys.stdout.write('aaaa')
+sys.stdout.writeline('bbbb') # 无换行输出
+# 错误输出
+sys.stderr.write('')
+
+# 退出程序,指定状态码
+sys.exit(0)
